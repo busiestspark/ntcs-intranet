7d0aad2e8afc83b547144bc633adf01c9857e721
Busiest_Spark <sataylor16123@upei.ca>
2026-07-03 18:19:49 -0300

    Quartz sync: Jul 3, 2026, 6:19 PM

--- quartz.ts
+++ quartz.ts
@@ -1,4 +1,7 @@
+import * as ExternalPlugin from "./.quartz/plugins"
 import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
+
+ExternalPlugin.Footer() 
 
 const config = await loadQuartzConfig()
 export default config
